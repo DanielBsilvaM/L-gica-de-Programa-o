384473a587f4e0e64101fb80525621c55ede7c58
DanielBsilvaM <50593970+DanielBsilvaM@users.noreply.github.com>
2019-05-13 15:37:41 -0300

    dados outros

--- 1info1-lgica/dados.py
+++ 1info1-lgica/dados.py
@@ -1,13 +1,16 @@
+# Programa dos dados 
+# Marco André Mendes <marcoandre@gmail.com>
+
 from random import randint
 
-print("Jogo de dados")
+print("Jogo de Dados(v 0.1)")
 
-dados1 = randint(1, 6)
+dados1 = randint(1, 6) 
 dados2 = randint(1, 6)
 dados3 = randint(1, 6)
 dados4 = randint(1, 6)
 
-print("Dados 1:", dados1)
-print("Dados 2:", dados2)
-print("Dados 3:", dados3)
-print("Dados 4:", dados4)
+print("Dado 1:", dados1)
+print("Dado 2:", dados2)
+print("Dado 3:", dados3)
+print("Dado 4:", dados4)
